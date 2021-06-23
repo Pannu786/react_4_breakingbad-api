da05ea9ac9a1f28c7d46935835a117d36db273d6
Pannu786 <thepannu786@gmail.com>
2021-06-23 22:10:41 +0530

    added smthing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/ui/Header';
 
 const App = () => {
+  const [items, setItems] = useState([])
+
   return (
     <div className='container'>
       <Header />
