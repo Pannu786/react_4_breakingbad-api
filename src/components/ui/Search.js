282e200295e949dffd99e808b7ec00c82a15784f
Pannu786 <thepannu786@gmail.com>
2021-06-25 23:45:29 +0530

    dne

--- src/components/ui/Search.js
+++ src/components/ui/Search.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
-const Search = () => {
-  return <div></div>;
+const Search = ({ getQuery }) => {
+  const [text, setText] = useState('');
+
+  const onChange = (q) => {
+    setText(q);
+    getQuery(q);
+  };
+  return (
+    <section className='search'>
+      <form>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Search Characters'
+          value={text}
+          onChange={(e) => onChange(e.target.value)}
+          autofocus
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Search;
